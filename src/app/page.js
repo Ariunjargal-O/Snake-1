fc2d9a14b9ca816743180b2fff05b2de8ebbc1ef
24LP2179 24LP2179 <24LP2179@G303-CC0147.local>
2025-02-25 13:11:30 +0800

    just keychange direction

--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,134 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useInterval } from "usehooks-ts";
-
-const size = 15;
+import { useEventListener } from "usehooks-ts";
+const size = 10;
 
 const board = {
-  width: 30,
-  height: 30,
+  width: 50,
+  height: 50,
 };
 
 export default function Home() {
   const [head, setHead] = useState({
-    top: 50,
-    left: 50,
+    top: 0,
+    left: 0,
+  });
+  const [direction, setDirection] = useState("right");
+
+  useEffect(() => {
+    document.addEventListener("keydown", (e) => {
+      if (e.code === "Arrowup") {
+        setDirection("up");
+      }
+      switch (e.code) {
+        case "ArrowUp": {
+          setDirection("up");
+          break;
+        }
+        case "ArrowDown": {
+          setDirection("down");
+          break;
+        }
+        case "ArrowLeft": {
+          setDirection("left");
+          break;
+        }
+        case "ArrowRight": {
+          setDirection("right");
+          break;
+        }
+      }
+    });
   });
 
+  function gameLoop() {
+    let newLeft = head.left;
+    let newTop = head.top;
+    switch (direction) {
+      case "right": {
+        newLeft = head.left + 1;
+        if (board.width <= newLeft) {
+          newLeft = 0;
+        }
+        break;
+      }
+      case "up": {
+        newTop = head.top - 1;
+        if (newTop < 0) {
+          newTop = board.height - 1;
+        }
+        break;
+      }
+      case "down": {
+        newTop = head.top + 1;
+        if (board.height <= newTop) {
+          newTop = 0;
+        }
+        break;
+      }
+      case "left": {
+        newLeft = head.left - 1;
+        if (newLeft < 0) {
+          newLeft = board.width - 1;
+        }
+        break;
+      }
+    }
+    setHead({ top: newTop, left: newLeft });
+  }
+
   useInterval(() => {
-    setHead({ top: head.top, left: head.left + 1 });
-  }, 50);
+    gameLoop();
+  }, 80);
 
   return (
     <div>
       <header className="text-center mt-10 text-[50px]">
-        Welcome to Snake by Ari
+        Welcome to Snake
       </header>
+      <div >
       <div
         style={{ width: board.width * size, height: board.height * size }}
-        className="bg-pink-300 relative mx-auto mt-20 border-2 border-black"
+        className="bg-pink-300 relative mx-auto mt-20 border-black"
       >
         <div
-          style={{ width: size, height: size, top:head.top, left: head.left }}
+          style={{
+            width: size,
+            height: size,
+            top: head.top * size,
+            left: head.left * size,
+          }}
           className="bg-stone-900 absolute rounded-full "
         ></div>
+      </div>
+      </div>
+
+      <div className="flex gap-5 mt-10 justify-center">
+        <button
+          onClick={() => setDirection("left")}
+          className="bg-blue-100 rounded-sm py-2 px-8"
+        >
+          Left
+        </button>
+        <button
+          onClick={() => setDirection("up")}
+          className="bg-blue-100 rounded-sm py-2 px-8"
+        >
+          Up
+        </button>
+        <button
+          onClick={() => setDirection("down")}
+          className="bg-blue-100 rounded-sm py-2 px-8"
+        >
+          Down
+        </button>
+        <button
+          onClick={() => setDirection("right")}
+          className="bg-blue-100 rounded-sm py-2 px-8"
+        >
+          Right
+        </button>
       </div>
     </div>
   );
